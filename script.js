79d4a1e650e1fc98bfcb925e2b0ec936f473e00b
isalima2408 <ill_bella@hotmail.com>
2023-11-03 21:39:43 -0400

    change profile image

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
     const img = document.querySelector("#perfil img")
     
     if(html.classList.contains("light")) {
-        img.setAttribute("src", "../img/subzero.png")
+        img.setAttribute("src", "https://static.vecteezy.com/system/resources/previews/020/911/729/non_2x/profile-icon-avatar-icon-user-icon-person-icon-free-png.png")
     } else {
-        img.setAttribute("src", "../img/scorpion.png")
+        img.setAttribute("src", "https://img.freepik.com/vetores-premium/icone-de-perfil-de-pessoas_24877-40756.jpg")
     }
 }
