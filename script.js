442af38f766e8a45266176956f085af917ea2c21
Isabella Lima <149109430+isalima2408@users.noreply.github.com>
2023-11-06 13:23:05 -0400

    Update script.js conclusao rocketseat

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle("light")
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-    const img = document.querySelector("#perfil img")
-    
-    if(html.classList.contains("light")) {
-        img.setAttribute("src", "https://static.vecteezy.com/system/resources/previews/020/911/729/non_2x/profile-icon-avatar-icon-user-icon-person-icon-free-png.png")
-    } else {
-        img.setAttribute("src", "https://img.freepik.com/vetores-premium/icone-de-perfil-de-pessoas_24877-40756.jpg")
-    }
+  const img = document.querySelector("#perfil img")
+
+  if(html.classList.contains("light")) {
+    img.setAttribute("src", "https://img.freepik.com/vetores-premium/icone-de-perfil-de-pessoas_24877-40756.jpg")
+  } else {
+    img.setAttribute("src", "https://cdn-icons-png.flaticon.com/512/5987/5987462.png")
+  }
 }
